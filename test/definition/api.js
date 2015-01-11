f3c729caf82221d19d24dde4661d1ea38ad65f09
Ivan Nikolić <niksy5@gmail.com>
2015-01-11 18:53:15 +0100

    Adjust definition

--- test/definition/api.js
+++ test/definition/api.js
@@ -1,3 +1,3 @@
-sortMediaQueries.sort(['screen and (min-width:900px)', 'screen and (min-width:1000px)']);
-sortMediaQueries.sortString(['screen and (min-width:900px)', 'screen and (min-width:1000px)']);
-sortMediaQueries.sortObject([{media:'screen and (min-width:900px)'},{media:'screen and (min-width:1000px)'}]);
+var smq = require('sort-media-queries');
+smq(['screen and (min-width:900px)', 'screen and (min-width:1000px)']);
+smq([{media:'screen and (min-width:900px)'},{media:'screen and (min-width:1000px)'}],'media');
